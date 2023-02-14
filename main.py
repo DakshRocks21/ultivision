e729faf5d395ea36c61d832fb12f6972fa72f3f3
tyzrichard <tyzrichard28@gmail.com>
2023-02-14 18:52:33 +0800

    main.py changes

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from MainApp.utils.dependencies import download_dependencies
 check_if_config_exists()
 
 # download dependencies
-download_dependencies()
+# download_dependencies()
 
 # download model
-from MainApp.utils.model import download_model
-download_model()
+# from MainApp.utils.model import download_model
+# download_model()
 
 #import appTest.audio as audio
 # test all the sound effects
-#audio.test()
+# audio.test()
 
 
 from MainApp.run import launchApp
